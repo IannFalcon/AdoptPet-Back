a5e0a75362323b7b023dafd22b78c22bc7b50f12
Iann <noseassapo@gmail.com>
2024-09-12 00:00:02 -0500

    Creacion del proyecto

---
+++ src/main/java/com/cibertec/adopta_una_mascota/AdoptaUnaMascotaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.adopta_una_mascota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaUnaMascotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaUnaMascotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/adopta_una_mascota/AdoptaUnaMascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.adopta_una_mascota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaUnaMascotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
